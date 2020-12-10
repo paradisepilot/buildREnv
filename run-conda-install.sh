962a7f5d9a99144747074b3592113520bf1e0cc7
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-12-09 19:01:56 -0500

    updated: run-conda-install.sh

--- run-conda-install.sh
+++ run-conda-install.sh
@@ -6,25 +6,28 @@ echo targetDIR=${targetDIR}
 
 conda create  --yes -p ${targetDIR}
 conda install --yes -p ${targetDIR} -c conda-forge \
-        "r-rstan" \
         "r-Cairo" \
-        "r-rJava" \
-        "r-magick" \
-        "r-gifski" \
-        "r-RMySQL" \
-        "r-jqr" \
-        "r-rjags" \
-        "r-rgdal" \
-        "r-pdftools" \
-        "r-RMariaDB" \
-        "r-rgl" \
-        "r-systemfonts" \
-        "r-R2jags" \
-        "r-textshaping" \
+        "r-EnvStats" \
+        "r-gdata" \
         "r-gdtools" \
-        "r-ragg" \
         "r-ggiraph" \
+        "r-ggpubr" \
+        "r-gifski" \
+        "r-pdftools" \
         "r-pkgdown" \
+        "r-R2jags" \
+        "r-rgdal" \
+        "r-ragg" \
+        "r-rgl" \
+        "r-rjags" \
+        "r-rJava" \
+        "r-RMariaDB" \
+        "r-jqr" \
+        "r-magick" \
+        "r-RMySQL" \
+        "r-rstan" \
+        "r-systemfonts" \
+        "r-textshaping" \
         "r-tidyverse" \
         "r-vdiffr" > stdout.conda-install 2> stderr.conda-install
 
