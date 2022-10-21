9297236f3feed01aacc492b918425e69ba8c8d28
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-10-20 21:20:35 -0400

    updated: run-conda-install.sh

--- run-conda-install.sh
+++ run-conda-install.sh
@@ -6,31 +6,40 @@ echo targetDIR=${targetDIR}
 
 conda create  --yes -p ${targetDIR}
 conda install --yes -p ${targetDIR} -c conda-forge \
-        "mysql" \ 
+      # "mysql" \ 
+        "r-arrow" \
         "r-bayesplot" \
         "r-Cairo" \
         "r-EnvStats" \
         "r-flexdashboard" \
+        "r-gdalutilities" \
+        "r-gdalutils" \
         "r-gdata" \
         "r-gdtools" \
         "r-ggiraph" \
         "r-ggpubr" \
         "r-gifski" \
+        "r-jqr" \
+        "r-magick" \
         "r-pdftools" \
         "r-pkgdown" \
         "r-R2jags" \
         "r-rgdal" \
         "r-ragg" \
+        "r-raster" \
+        "r-rastervis" \
         "r-rgl" \
         "r-rjags" \
         "r-rJava" \
-        "r-RMariaDB" \
-        "r-jqr" \
-        "r-magick" \
         "r-rjson" \
+        "r-RMariaDB" \
         "r-RMySQL" \
         "r-rstan" \
+        "r-rstoolbox" \
+        "r-sf" \
+        "r-sp" \
         "r-systemfonts" \
+        "r-terra" \ 
         "r-textshaping" \
         "r-tidyverse" \
         "r-tiff" \
